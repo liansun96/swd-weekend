b4d2e53ab87506d9ab05680e6004abf24799f201
MMS Mac 1 <mmsmac1@MMSs-iMac.local>
2021-06-16 10:01:44 +0630

    update

---
+++ data.js
@@ -0,0 +1,40 @@
+let fruit = ["apple","orange","mango","banan","grab"];
+let money = [500,125,300,100,900,70,60,5];
+let students = [
+    {
+        name : "hein htet zan",
+        age : 26,
+        gender : "male",
+        mark : 75
+    },
+    {
+        name : "Kyaw Gyi",
+        age : 23,
+        gender : "male",
+        mark : 37
+    },
+    {
+        name : "Wah Wah",
+        age : 25,
+        gender : "female",
+        mark : 60
+    },
+    {
+        name : "zaw zaw",
+        age : 19,
+        gender : "male",
+        mark : 95
+    },
+    {
+        name : "may thu",
+        age : 18,
+        gender : "female",
+        mark : 81
+    },
+    {
+        name : "Mg Kaung",
+        age : 29,
+        gender : "male",
+        mark : 40
+    },
+];
